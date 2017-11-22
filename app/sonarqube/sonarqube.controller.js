d5cd95b1a7ed6b157f40e034a8fe2eaf60b38e03
Agarwal <subham.agarwal@capgemini.com>
2017-11-22 11:06:13 +0530

    updated sonar url,analysis url, colours

--- app/sonarqube/sonarqube.controller.js
+++ app/sonarqube/sonarqube.controller.js
@@ -8,6 +8,6 @@
 	SonarController.$inject = ['$scope','$location','$sce'];
 
 	function SonarController ($scope,$location,$sce) {
-		$scope.sonarUrl = $sce.trustAsResourceUrl($location.protocol()+"://dadc.fs.capgemini.com:9000");
+		$scope.sonarUrl = $sce.trustAsResourceUrl($location.protocol()+"://10.207.166.31:9000");
 	}
 })();
